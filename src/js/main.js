29e38c2efebde8ff493b9b2ce0ac6ca6884b3840
Sergey <SergeyChudinov1986@gmail.com>
2022-10-11 15:30:48 +0400

    Make lesson-2-06

--- src/js/main.js
+++ src/js/main.js
@@ -3,16 +3,19 @@ import sliders from './modules/sliders';
 import forms from './modules/forms';
 import mask from './modules/mask';
 import checkTextInputs from './modules/checkTextInputs';
+import showMoreStyles from './modules/showMoreStyles';
 
 window.addEventListener('DOMContentLoaded', () => {
     "use strict";
 
     modals();
-    sliders('.feedback-slider-item', '', '.main-prev-btn', '.main-next-btn');
     sliders('.main-slider-item', 'vertical');
+    sliders('.feedback-slider-item', '', '.main-prev-btn', '.main-next-btn');
     forms();
     mask('[name="phone"]');
     checkTextInputs('[name="name"]');
     checkTextInputs('[name="message"]');
+    showMoreStyles('.button-transparent', '.styles-2')
+
 });
 
